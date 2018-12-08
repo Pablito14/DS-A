5aca7fcde055c6506dc49399e30e971591df9ba3
pablo <pablo.rosales14@gmail.com>
2018-12-07 23:32:36 -0800

    Logic is fine for the most part, Having editor issues trying to run testing to see if it works

---
+++ src/test/java/ArrayReverseTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArrayReverseTest {
+
+    @Test
+    public void reverseArray() {
+        int[] example = {0,1,2,3};
+        int[] answer = reverseArray(example);
+        assertArrayEquals("Should flip array from 0,1,2,3",  );
+    }
+}
