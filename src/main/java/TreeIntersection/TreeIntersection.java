7aa4b40cde2eec5c5a4ed7b59fec1c6d041beef4
pablo <pablo.rosales14@gmail.com>
2019-01-16 21:34:33 -0800

    Aye dios mio

--- src/main/java/TreeIntersection/TreeIntersection.java
+++ src/main/java/TreeIntersection/TreeIntersection.java
@@ -9,23 +9,23 @@ import java.util.HashSet;
 
 public class TreeIntersection {
 
-  public int[] treeIntersection(Tree<Node<Integer>> a, Tree<Node<Integer>> b){
+//  public int[] treeIntersection(Tree<Node<Integer>> a, Tree<Node<Integer>> b){
+//
+//    //data structures to use
+//    HashSet<Integer> seen;
+//    ArrayList<Integer> dupelicateList;
+//    int[] dupelicateArray;
 
-    //data structures to use
-    HashSet<Integer> seen;
-    ArrayList<Integer> dupelicateList;
-    int[] dupelicateArray;
+//    //traverse through the first tree adding all the nodes values to a hash set
 
-    //traverse through the first tree adding all the nodes values to a hash set
+//    //once the first tree has been processed, check each node in the second tree to see if it has been seen,
+//      // if it has been seen, add it to the arrraylist and proceed
+//      // if it hasnt been seen, add it to the hash set and proceed
+//    //once all nodes have been processed we need to convert the arrayList into an array
 
-    //once the first tree has been processed, check each node in the second tree to see if it has been seen,
-      // if it has been seen, add it to the arrraylist and proceed
-      // if it hasnt been seen, add it to the hash set and proceed
-    //once all nodes have been processed we need to convert the arrayList into an array
-
-  //BUS DRIVER, MOVE THAT BUS!
-    return dupelicateArray;
-  }
+//  //BUS DRIVER, MOVE THAT BUS!
+//    return dupelicateArray;
+//  }
 
 
 }
