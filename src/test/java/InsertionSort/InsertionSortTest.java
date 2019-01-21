97562257155d2b0a01d77bce34392470944f388b
pablo <pablo.rosales14@gmail.com>
2019-01-21 11:27:20 -0800

    I cant get my read me to link down to the documentation, but its there at the bottom

--- src/test/java/InsertionSort/InsertionSortTest.java
+++ src/test/java/InsertionSort/InsertionSortTest.java
@@ -9,8 +9,9 @@ public class InsertionSortTest {
 
   @Test
   public void insertionSortTest() {
-    int[] myArr = {6,2,8,1,4,5,3,7,9,0};
-    int[] sorted = {0,1,2,3,4,5,6,7,8,9};
+    int[] myArr = {6,2,-1,8,1,4,5,0,3,7,9,0};
+    int[] sorted = {-1,0,0,1,2,3,4,5,6,7,8,9};
     assertArrayEquals("Should return sorted",sorted, insertionSort(myArr));
+
   }
 }
